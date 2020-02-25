09e724bb66078e3592e38b97938058a0c9c2951d
ys238 <ys238@duke.edu>
2020-02-24 22:29:37 -0500

    init commit

---
+++ NewsstandHW6/NewsstandHW6/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NewsstandHW6
+//
+//  Created by student on 2/24/20.
+//  Copyright © 2020 student. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
