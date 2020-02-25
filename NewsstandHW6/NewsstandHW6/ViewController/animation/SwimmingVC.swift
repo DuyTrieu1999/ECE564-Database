f3778f76bd7514c20057491f0d556582491ba493
cl518@duke.edu <cl518@duke.edu>
2020-02-25 18:17:33 -0500

    animation_cl

---
+++ NewsstandHW6/NewsstandHW6/ViewController/animation/SwimmingVC.swift
@@ -0,0 +1,30 @@
+//
+//  SwimmingVC.swift
+//  NewsstandHW6
+//
+//  Created by student on 2/25/20.
+//  Copyright © 2020 student. All rights reserved.
+//
+
+import UIKit
+
+class SwimmingVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
