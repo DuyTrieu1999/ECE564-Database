ff0a298de09d4b6859b60b4e6ebde2ce70010e8b
dvt5 <dvt5@duke.edu>
2020-02-25 21:21:39 -0500

    add log in layout

--- NewsstandHW6/NewsstandHW6/ViewController/StartViewController.swift
+++ NewsstandHW6/NewsstandHW6/ViewController/StartViewController.swift
@@ -7,26 +7,62 @@
 //
 
 import UIKit
+import shibauthframework2019
 
 class StartViewController: UIViewController {
-
+    
     @IBOutlet var LogoImg: UIImageView!
+        
+    @IBOutlet var LogInButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        setUpView()
+    }
+    func setUpView() {
         LogoImg.image = UIImage(named: "AppIcon")
+        self.view.backgroundColor = UIColorFromHex(rgbValue: 0x012169,alpha: 1)
+        self.LogInButton.addTarget(self, action: #selector(logIn), for: .touchUpInside)
+    }
+    func UIColorFromHex(rgbValue:UInt32, alpha:Double=1.0)->UIColor {
+        let red = CGFloat((rgbValue & 0xFF0000) >> 16)/256.0
+        let green = CGFloat((rgbValue & 0xFF00) >> 8)/256.0
+        let blue = CGFloat(rgbValue & 0xFF)/256.0
+
+        return UIColor(red:red, green:green, blue:blue, alpha:CGFloat(alpha))
+    }
+    @objc func logIn() {
+        let alertController = LoginAlert(title: "Authenticate", message: nil, preferredStyle: .alert)
+        alertController.delegate = self
+        self.present(alertController, animated: true, completion: nil)
+    }
+
+}
+extension StartViewController: LoginAlertDelegate {
+    
+    func onSuccess(_ loginAlertController: LoginAlert, didFinishSucceededWith status: LoginResults, netidLookupResult: NetidLookupResultData?, netidLookupResultRawData: Data?, cookies: [HTTPCookie]?, lastLoginTime: Date) {
+        // succeeded, extract netidLookupResult.id and netidLookupResult.password for your server credential
+        // other properties needed for homework are also in netidLookupResult
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func onFail(_ loginAlertController: LoginAlert, didFinishFailedWith reason: LoginResults) {
+        // when authentication fails, this method will be called.
+        // default implementation provided
     }
-    */
+    
+    func inProgress(_ loginAlertController: LoginAlert, didSubmittedWith status: LoginResults) {
+        // this method will get called for each step in progress.
+        // default implementation provided
+    }
+    
+    func onLoginButtonTapped(_ loginAlertController: LoginAlert) {
+        // the login button on the alert is tapped
+        // default implementation provided
+    }
 
+    func onCancelButtonTapped(_ loginAlertController: LoginAlert) {
+        // the cancel button on the alert is tapped
+        // default implementation provided
+    }
 }
