9dfd091120cb636ae71bbd20fa9c6c2b68c2ecaf
cl518@duke.edu <cl518@duke.edu>
2020-03-05 20:51:53 -0500

    post_all_data

--- NewsstandHW6/NewsstandHW6/DataModel/DataGroup.swift
+++ NewsstandHW6/NewsstandHW6/DataModel/DataGroup.swift
@@ -85,14 +85,7 @@ func createDukePersons() -> [DukePerson]{
     else
     {
         createPerson = downloadServer()
-        //let initperson = initializeDukePerson()
-        //createPerson.append(contentsOf: initperson)
-        //dataEncoder()
-        for i in initializeDukePerson()
-        {
-            uploadnewperson(newperson: i)
-        }
-        
+       // dataEncoder()
     }
     return createPerson
 }
@@ -242,16 +235,10 @@ func getPersonData(personid:String) -> DukePerson
 func initializeDukePerson() -> [DukePerson] {
     // dukePerson entries
     let myself = DukePerson(firstName: "Yuqin", lastName: "Shen", whereFrom: "Guangdong, China", gender: .Female, hobbies: ["yogo", "reading", "Pili dramas"], role: .Student, degree: .MS, languages: ["C++", "Python", "Java"], picture: UIImage(imageLiteralResourceName: "myself"), team: "Newsstand", netid: "ys238", email: "ys238@duke.edu", department: "Pratt School of Engineering", id: "ys238", nextPage: "Reading", isFavourite: false)
-    let professor =  DukePerson(firstName: "Ric", lastName: "Telford", whereFrom: "Chatham County, NC", gender: .Male, hobbies: ["swimming", "biking", "hiking"], role: .Professor, degree: .BS, languages: ["Swift", "C", "C++"], picture: UIImage(imageLiteralResourceName: "Ric"), team: "Professor", netid: "rt113", email: "rt113@duke.edu", department: "Pratt School of Engineering", id: "rt113", nextPage: "no", isFavourite: false)
-    let TA1 = DukePerson(firstName: "Jingru", lastName: "Gao", whereFrom: "Shanghai, China", gender: .Female, hobbies: ["traveling", "reading", "movies"], role: .TA, degree: .MS, languages: ["Swift", "C++", "Python"], picture: UIImage(imageLiteralResourceName: "Jingru"), team: "TA", netid: "jg404", email: "jg404@duke.edu", department: "Pratt School of Engineering", id: "jg404", nextPage: "no", isFavourite: false)
-    let TA2 = DukePerson(firstName: "Haohong", lastName: "Zhao", whereFrom: "Hebei, China", gender: .Male, hobbies: ["reading", "jogging"], role: .Student, degree: .MS, languages: ["Swift", "Python"], picture: UIImage(imageLiteralResourceName: "haohong"), team: "TA", netid: "hz147", email: "hz147@duke.edu", department: "Pratt School of Engineering", id: "hz147", nextPage: "no", isFavourite: false)
-
-    // add more two persons
-
     let CongLi: DukePerson = DukePerson(firstName: "Cong", lastName: "Li", whereFrom: "Shanxi, China", gender: .Male, hobbies: ["traveling", "reading"], role: .Student, degree: .MS, languages: ["C++", "Java"], picture: UIImage(imageLiteralResourceName: "congli"), team: "Newsstand", netid: "cl518", email: "cl518@duke.edu", department: "Pratt School of Engineering", id: "cl518", nextPage: "Swimming", isFavourite: false)
     let QiruiHe: DukePerson = DukePerson(firstName: "Qirui", lastName: "He", whereFrom: "Guangxi, China", gender: .Male, hobbies: ["Basketball"], role: .Student, degree: .MS, languages: ["C++", "Python", "Java"], picture: UIImage(imageLiteralResourceName: "qirui"), team: "Newsstand", netid: "qh37", email: "qh37@duke.edu", department: "Pratt School of Engineering", id: "qh37", nextPage: "Qirui", isFavourite: false)
     let TrieuDuy: DukePerson = DukePerson(firstName: "Trieu", lastName: "Duy", whereFrom: "Hanoi, Vietnam", gender: .Male, hobbies: ["traveling", "cooking", "coding"], role: .Student, degree: .BS, languages: ["JS", "Python", "Java"], picture: UIImage(imageLiteralResourceName: "duytrieu"), team: "Newsstand", netid: "dvt5", email: "dvt5@duke.edu", department: "Trinity School", id: "dvt5", nextPage: "world", isFavourite: false)
-    let dukePersons: [DukePerson] = [myself, professor, TA1, TA2, CongLi, QiruiHe, TrieuDuy]
+    let dukePersons: [DukePerson] = [myself, CongLi, QiruiHe, TrieuDuy]
     return dukePersons
 }
 var dukePersons:[DukePerson] = createDukePersons()
@@ -259,16 +246,21 @@ var sections: [GroupSection] = createSections(personArray: dukePersons)
 
 func uploadnewperson(newperson: DukePerson)
 {
-    
-    let url=URL(string:"https://rt113-dt01.egr.duke.edu:5640/entries")
+    let username = "cl518"
+       let password = "CB35F547EC349DA119EDFAE17566500E"
+    let loginData = String(format: "%@:%@", username, password).data(using: String.Encoding.utf8)
+    let base64LoginData = loginData!.base64EncodedString()
+       // create the request
+    let url=URL(string:"https://rt113-dt01.egr.duke.edu:5640/b64entries")
     guard let requesturl = url else {fatalError()}
     var request = URLRequest(url: requesturl)
+    request.setValue("Basic \(base64LoginData)", forHTTPHeaderField: "Authorization")
     request.httpMethod = "POST"
     request.setValue("application/json",forHTTPHeaderField: "Accept")
     request.setValue("application/json", forHTTPHeaderField: "Content-Type")
-    let imgData:Data = newperson.picture.jpegData(compressionQuality: 0.2)!
-    //let imgData:Data = newperson.picture.pngData()!
-    let img_64data:String=imgData.base64EncodedString()
+    let picImage = newperson.picture
+    let picImageData: Data = picImage.jpegData(compressionQuality: 0.2)!
+    let picBase64 = picImageData.base64EncodedString()
     let newdictionary: [String: Any] =
     [
     "id": newperson.id,
@@ -284,10 +276,9 @@ func uploadnewperson(newperson: DukePerson)
     "languages": newperson.languages,
     "department": newperson.department,
     "email": newperson.email,
-    "picture":img_64data
+    "picture":picBase64
     ]
     let jsonData = try? JSONSerialization.data(withJSONObject: newdictionary,options: .fragmentsAllowed)
-    //let jsonString = NSString(data:jsonData! , encoding: String.Encoding.utf8.rawValue)
     request.httpBody = jsonData
     _ = URLSession.shared.dataTask(with: request){(data,response,error) in
         if (error != nil){
@@ -304,8 +295,6 @@ func uploadnewperson(newperson: DukePerson)
             if let data = data, let dataString = String(data: data, encoding: .utf8) {
                           print("Response data string:\n \(dataString)")
                     }
-            
-            
         }
         
     }.resume()
